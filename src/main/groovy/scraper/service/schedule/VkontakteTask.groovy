037c89826f096a112e955e08d92f75862bdd08b2
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2017-07-29 03:01:09 +0300

    major changed logger, pipeline builder logic

--- src/main/groovy/scraper/service/schedule/VkontakteTask.groovy
+++ src/main/groovy/scraper/service/schedule/VkontakteTask.groovy
@@ -3,7 +3,6 @@ package scraper.service.schedule
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import scraper.core.browser.Browser
 import scraper.core.browser.BrowserFactory

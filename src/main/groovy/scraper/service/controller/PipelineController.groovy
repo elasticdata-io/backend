ace6d75b058f45cb2a2105e0762ac96bf7b90a6f
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2017-08-13 19:30:09 +0300

    added browser address

--- src/main/groovy/scraper/service/controller/PipelineController.groovy
+++ src/main/groovy/scraper/service/controller/PipelineController.groovy
@@ -197,13 +197,13 @@ class PipelineController {
      */
     private Browser getPipelineBrowser(Pipeline pipelineEntity) {
         def factory = new BrowserFactory()
+        def config = []
+        if (pipelineEntity.browserAddress) {
+            config = [browserAddress: pipelineEntity.browserAddress]
+        }
         if (pipelineEntity && pipelineEntity.browser) {
-            def config = []
-            if (pipelineEntity.browserAddress) {
-                config = [browserAddress: pipelineEntity.browserAddress]
-            }
             return factory.createFromString(pipelineEntity.browser, config)
         }
-        return factory.createFromClass(DEFAULT_BROWSER)
+        return factory.createFromClass(DEFAULT_BROWSER, config)
     }
 }

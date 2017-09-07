966272dd6f96ff56d5442bbb4a13bf5995607451
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2017-09-08 01:45:51 +0300

    disabled public rest api

--- src/main/groovy/scraper/service/Application.java
+++ src/main/groovy/scraper/service/Application.java
@@ -3,6 +3,7 @@ package scraper.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,7 +11,7 @@ import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = RepositoryRestMvcAutoConfiguration.class)
 @ComponentScan
 @EnableScheduling
 public class Application {

4f1807543bd076ffeccfe2ef0fba1b5fb7f81182
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2017-09-17 18:47:31 +0300

    added new driver

--- src/main/groovy/scraper/service/pipeline/PipelineService.groovy
+++ src/main/groovy/scraper/service/pipeline/PipelineService.groovy
@@ -34,7 +34,7 @@ class PipelineService {
 
     private DefaultListableBeanFactory beanFactory
 
-    private String RUN_DIRECTORY = '/tmp/scraper-service'
+    private String RUN_DIRECTORY = '/tmp/scraper-resolve'
     private Class DEFAULT_BROWSER = Phantom.class
 
     @Autowired

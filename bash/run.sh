5675cc733e05460d1b001967545f2552229a585e
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2017-10-03 02:30:12 +0300

    fixed depends

--- bash/run.sh
+++ bash/run.sh
@@ -4,4 +4,4 @@ java -jar /Users/s/Downloads/selenium-server-standalone-3.4.0.jar -role hub -bro
 java -jar /Users/s/Downloads/selenium-server-standalone-3.4.0.jar -role node -browser browserName=chrome &
 cd /Users/s/sites/scraper-service-ui/app && npm run dev
 
-java -jar /Users/s/Downloads/selenium-server-standalone-3.4.0.jar -role node -browser browserName=chrome -port 4449 -hub http://selenium.bars-parser.com:4444/grid/register -host 37.57.124.190
+java -jar /Users/s/Downloads/selenium-server-standalone-3.5.3.jar -role node -browser browserName=chrome -port 4449 -hub http://selenium.bars-parser.com:4444/grid/register -host 37.57.124.190

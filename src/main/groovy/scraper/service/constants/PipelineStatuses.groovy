faa794a5a27ae036b5ff85ee917b79ac06337741
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2018-03-08 23:20:14 +0200

    added waiting pipeline status

--- src/main/groovy/scraper/service/constants/PipelineStatuses.groovy
+++ src/main/groovy/scraper/service/constants/PipelineStatuses.groovy
@@ -3,6 +3,7 @@ package scraper.service.constants
 class PipelineStatuses {
 
     public static NOT_RUNNING = 'not running'
+    public static WAITING = 'waiting'
     public static RUNNING = 'running'
     public static COMPLETED = 'completed'
     public static ERROR = 'error'

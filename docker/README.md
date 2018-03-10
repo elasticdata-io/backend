cac98b9ba47a5f5d8726c3698bab2af6aa52f074
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2018-03-10 04:13:27 +0200

    added waiting pipeline status

--- docker/README.md
+++ docker/README.md
@@ -8,3 +8,23 @@ http://localhost:15673 - rabbitmq admin plugin started
 docker container stop http-service mongo rabbitmq
 
 docker rm http-service mongo rabbitmq
+
+
+docker run --rm --name mongo -d mongo
+
+docker exec -it mongo bash
+
+mongorestore --db scraper-service --drop /tmp/backup/
+
+docker container stop mongo
+
+docker run --rm --name mongo -d mongo --auth
+
+docker exec -it mongo mongo admin
+
+db.createUser({ user: 'bombascter', pwd: '!Prisoner31!', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
+
+docker run -it --rm --link mongo:mongo mongo mongo -u bombascter -p '!Prisoner31!' --authenticationDatabase admin mongo/scraper-service
+
+
+    
--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-docker run -d --net net1 --ip 172.18.0.11 --hostname mongo  --name mongo mongo
-docker run -d --net net1 --ip 172.18.0.12 --hostname http-service --add-host rabbitmq:172.18.0.13 --add-host mongo:172.18.0.11 -p 8080:8080 --name http-service http-service
-docker run -d --net net1 --ip 172.18.0.13 --hostname rabbitmq -p 15673:15672 --name rabbitmq rabbitmq:3-management
+docker run -d --net net1 --ip 172.18.0.11 --hostname mongo -p 27017:27017 --name mongo mongo
+docker run -d --net net1 --ip 172.18.0.13 --hostname rabbitmq -p 15673:15672 -p 5672:5672 --name rabbitmq rabbitmq
+docker run -d --net net1 --ip 172.18.0.12 --hostname http-service --add-host rabbitmq:172.18.0.13 \
+    --add-host mongo:172.18.0.11 -p 8080:8080 --name http-service http-service

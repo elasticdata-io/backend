78d6af77ba2f0366c59205f577893955b5aff3c8
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2018-03-10 23:57:22 +0200

    added waiting pipeline status

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # run this script from parent directory
-docker build -t http-service -f docker/http-service .
-docker build -t mongo -f docker/mongo .
-docker build -t rabbitmq -f docker/rabbitmq .
+docker build --no-cache -t http-service -f docker/http-service .
+docker build --no-cache -t mongo -f docker/mongo .
+docker build --no-cache -t rabbitmq -f docker/rabbitmq .
 
 docker network create --subnet=172.18.0.0/16 net1

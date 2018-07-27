578936ad77f3b0f5250aee2cba687bd17c4035bf
admin <admin@TeamCity>
2018-07-27 20:12:27 +0000

    TeamCity change in 'Scraper Service' project: VCS roots of 'docker build' build configuration were updated

--- .teamcity/patches/buildTypes/DockerBuild.kts
+++ .teamcity/patches/buildTypes/DockerBuild.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("DockerBuild")
     name = "docker build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 }))
 

1a8c3fc940c9ffdb3cad0505295b991be54079ff
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2018-07-27 22:02:35 +0300

    docker push image teamcity

---
+++ docker/buildAndPublish.sh
@@ -0,0 +1,9 @@
+docker build -t build-jar -f docker/build-jar .
+docker container create --name extract build-jar
+docker container cp extract:/src/build/libs/scraper-service.jar ./scraper-service.jar
+docker container rm -f extract
+docker image rm build-jar
+docker image rm -f bombascter/http-service
+docker build -t bombascter/http-service -f docker/build-service .
+docker login -u bombascter -p '!Prisoner31!'
+docker push bombascter/http-service

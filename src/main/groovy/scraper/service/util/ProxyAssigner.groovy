855c41d50d7d0d284fd8fb652a00d7eb79b6f84f
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2018-07-29 15:41:33 +0300

    added intergation with proxy

--- src/main/groovy/scraper/service/util/ProxyAssigner.groovy
+++ src/main/groovy/scraper/service/util/ProxyAssigner.groovy
@@ -1,24 +1,20 @@
 package scraper.service.util
 
-import groovyx.net.http.HTTPBuilder
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import scraper.service.proxy.ProxyService
 
 @Component
 class ProxyAssigner {
 
-    String getProxy() {
-        return null
-        def http = new HTTPBuilder('http://list.didsoft.com')
-        def query = [email : 'bombascter@gmail.com', pass: '!Prisoner31!', pid: 'httppremium']
-        def html = http.get( path : '/get', query : query ) as StringReader
-        String text = html.text
-        def list = text.split("\n") as ArrayList<String>
-        def last = list.sort().last()
-        return normalize(last)
-    }
+    @Autowired
+    ProxyService proxyService
 
-    String normalize(String ip) {
-        return null
-        return ip.replaceAll("#.*", "")
+    String getProxy() {
+        def proxy = proxyService.getFastProxy()
+        if (!proxy) {
+            return null
+        }
+        return "${proxy.host}:${proxy.port}"
     }
 }

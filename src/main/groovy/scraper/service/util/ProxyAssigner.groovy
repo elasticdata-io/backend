fce8ec3a89d1ab01911c8242294b53adc6f5641e
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2018-07-29 17:07:10 +0300

    added intergation with proxy

--- src/main/groovy/scraper/service/util/ProxyAssigner.groovy
+++ src/main/groovy/scraper/service/util/ProxyAssigner.groovy
@@ -1,5 +1,7 @@
 package scraper.service.util
 
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import scraper.service.proxy.ProxyService
@@ -7,14 +9,21 @@ import scraper.service.proxy.ProxyService
 @Component
 class ProxyAssigner {
 
+    private Logger logger = LogManager.getRootLogger()
+
     @Autowired
     ProxyService proxyService
 
     String getProxy() {
-        def proxy = proxyService.getFastProxy()
-        if (!proxy) {
+        try {
+            def proxy = proxyService.getFastProxy()
+            if (!proxy) {
+                return null
+            }
+            return "${proxy.host}:${proxy.port}"
+        } catch (e) {
+            logger.error(e)
             return null
         }
-        return "${proxy.host}:${proxy.port}"
     }
 }

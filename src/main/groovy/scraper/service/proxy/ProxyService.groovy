f0f1132d782e7eb66c1d9fc195590aaedbf5584f
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2018-07-30 00:34:27 +0300

    proxy logger

--- src/main/groovy/scraper/service/proxy/ProxyService.groovy
+++ src/main/groovy/scraper/service/proxy/ProxyService.groovy
@@ -1,11 +1,15 @@
 package scraper.service.proxy
 
 import groovy.json.JsonSlurper
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class ProxyService {
+
+    private Logger logger = LogManager.getRootLogger()
 
     @Value('${proxy.service.url}')
     String proxyServiceUrl
@@ -16,10 +20,12 @@ class ProxyService {
     ProxyModel getFastProxy() {
         def data = new URL(proxyServiceUrl).getText(connectTimeout: CONNECT_TIMEOUT, readTimeout: READ_TIMEOUT)
         if (!data) {
+            logger.error("proxy not found, proxyServiceUrl: ${proxyServiceUrl}")
             return null
         }
         def jsonSlurper = new JsonSlurper()
         def object = jsonSlurper.parseText(data)
+        logger.info("proxy found")
         return object as ProxyModel
     }
 }

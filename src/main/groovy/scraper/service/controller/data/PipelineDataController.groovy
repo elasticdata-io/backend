7ce4357c3d45d1afa955eaeb89e6bcbaf90ce1c8
sergey.tkachenko <serg.tkachenko@hotmail.com>
2019-09-26 23:14:53 +0300

    default selenium env

--- src/main/groovy/scraper/service/controller/data/PipelineDataController.groovy
+++ src/main/groovy/scraper/service/controller/data/PipelineDataController.groovy
@@ -1,6 +1,7 @@
 package scraper.service.controller.data
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,14 +20,16 @@ import scraper.service.service.PipelineService
 import scraper.service.service.UserService
 
 import javax.servlet.http.HttpServletRequest
-import javax.xml.ws.http.HTTPException
 
 @RestController
 @RequestMapping("/api/pipeline")
 class PipelineDataController {
 
-    public static final String DEFAULT_BROWSER_ADDRESS = 'http://selenoid:4444/wd/hub'
-    public static final String DEFAULT_BROWSER = 'chrome'
+    @Value('${selenium.default.address}')
+    String SELENIUM_DEFAULT_ADDRESS
+
+    @Value('${selenium.default.browser}')
+    String SELENIUM_DEFAULT_BROWSER
 
     @Autowired
     UserTokenRepository userTokenRepository
@@ -98,8 +101,8 @@ class PipelineDataController {
         boolean isTakeScreenshot = request.getParameter('isTakeScreenshot') == "true" ?: false
         boolean isDebugMode = request.getParameter('isDebugMode') == "true" ?: false
         boolean needProxy = request.getParameter('needProxy') == "true" ?: false
-        String browserAddress = request.getParameter('browserAddress') ?: DEFAULT_BROWSER_ADDRESS
-        String browser = request.getParameter('browser') ?: DEFAULT_BROWSER
+        String browserAddress = request.getParameter('browserAddress') ?: SELENIUM_DEFAULT_ADDRESS
+        String browser = request.getParameter('browser') ?: SELENIUM_DEFAULT_BROWSER
         Integer runIntervalMin = (request.getParameter('runIntervalMin') ?: null) as Integer
         if (pipeline) {
             pipeline.browser = browser
@@ -117,7 +120,7 @@ class PipelineDataController {
             return pipeline
         }
         PipelineStatus status = pipelineStatusRepository.findByTitle('not running')
-        pipeline = new Pipeline(key: key, browser: DEFAULT_BROWSER, jsonCommands: jsonCommands,
+        pipeline = new Pipeline(key: key, browser: SELENIUM_DEFAULT_BROWSER, jsonCommands: jsonCommands,
                 user: user, description: description, dependOn: dependOnPipeline, createdOn: new Date(),
                 modifiedOn: new Date(), status: status, runIntervalMin: runIntervalMin,
                 isTakeScreenshot: isTakeScreenshot, isDebugMode: isDebugMode,

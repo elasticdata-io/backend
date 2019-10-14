d51d4ff2aead0060bd35ca79bf282a78bba8e5ea
sergey.tkachenko <serg.tkachenko@hotmail.com>
2019-10-14 13:50:22 +0300

    put to elasticsearch with document hash

--- src/main/groovy/scraper/service/elastic/ElasticSearchService.groovy
+++ src/main/groovy/scraper/service/elastic/ElasticSearchService.groovy
@@ -12,6 +12,7 @@ import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestClientBuilder
 import org.elasticsearch.client.RestHighLevelClient
 import org.elasticsearch.client.indices.CreateIndexRequest
+import org.elasticsearch.client.indices.GetIndexRequest
 import org.elasticsearch.common.xcontent.XContentType
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -47,21 +48,42 @@ class ElasticSearchService {
         if (!client) {
             return
         }
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest(index)
-        client
-                .indices()
-                .create(createIndexRequest, RequestOptions.DEFAULT)
-
+        createEsIndex(index)
         BulkRequest bulkRequest = new BulkRequest()
-        list.each { data ->
+        list.each { doc ->
+            String id = getDocumentIdHash(doc)
             IndexRequest request = new IndexRequest(index)
-            String json = new JsonBuilder(data).toString()
+            String json = new JsonBuilder(doc).toString()
             request.source(json, XContentType.JSON)
+            request.id(id)
             bulkRequest.add(request)
         }
         BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT)
         if (bulkResponse.hasFailures()) {
             logger.error(bulkResponse.buildFailureMessage())
         }
+    }
+
+    private void createEsIndex(String index) {
+        if (esIndexExists(index)) {
+            return
+        }
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest(index)
+        client
+                .indices()
+                .create(createIndexRequest, RequestOptions.DEFAULT)
+    }
+
+    private boolean esIndexExists(String index) {
+        GetIndexRequest request = new GetIndexRequest(index)
+        return client.indices().exists(request, RequestOptions.DEFAULT)
+    }
+
+    private String getDocumentIdHash(HashMap<String, String> doc) {
+        def all = ''
+        doc.each{ key, value ->
+            all += "${key}${value}"
+        }
+        return all.md5()
     }
 }

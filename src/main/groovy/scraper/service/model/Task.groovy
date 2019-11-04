0552ef2e13d47a3a30830d04e487f91ae233bd07
sergey.tkachenko <serg.tkachenko@hotmail.com>
2019-11-04 23:12:09 +0200

    add indexes for task entity

--- src/main/groovy/scraper/service/model/Task.groovy
+++ src/main/groovy/scraper/service/model/Task.groovy
@@ -3,9 +3,13 @@ package scraper.service.model
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.index.IndexDirection
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "task")
+@CompoundIndex(def = "{'userId':1, 'status':-1}", name = "user_id__status")
 class Task {
 
     @Id
@@ -16,6 +20,7 @@ class Task {
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     public Date endOnUtc
 
+    @Indexed(name = "pipeline_id_index", direction = IndexDirection.DESCENDING)
     public String pipelineId
     public String userId
 

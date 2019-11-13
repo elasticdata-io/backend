58ad39e48d5f17aa61c2f4853ae6bd4597e5ab10
sergey.tkachenko <serg.tkachenko@hotmail.com>
2019-11-13 22:39:44 +0200

    enter by user

--- src/main/groovy/scraper/service/controller/LoginController.groovy
+++ src/main/groovy/scraper/service/controller/LoginController.groovy
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
-import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
--- src/main/groovy/scraper/service/controller/UserController.groovy
+++ src/main/groovy/scraper/service/controller/UserController.groovy
@@ -4,12 +4,15 @@ import org.apache.log4j.LogManager
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import scraper.service.dto.mapper.UserMapper
 import scraper.service.dto.model.user.UserDto
+import scraper.service.model.UserToken
 import scraper.service.service.UserService
+import scraper.service.service.UserTokenService
 
 @RestController
 @RequestMapping("/user")
@@ -18,12 +21,35 @@ class UserController {
     private static Logger logger = LogManager.getLogger(UserController.class)
 
     @Autowired
-    UserService userService
+    private UserService userService
+
+    @Autowired
+    private UserTokenService userTokenService
 
     @GetMapping("/current")
     UserDto current(@RequestHeader("token") String token) {
         def user = userService.findByToken(token)
         def userDto = UserMapper.toUserDto(user)
         return userDto
+    }
+
+    @GetMapping('/token/{userId}')
+    String loginByUser(@PathVariable String userId, @RequestHeader("token") String token) {
+        def user = userService.findByToken(token)
+        if (!user) {
+            return
+        }
+        if (!user.isAdmin) {
+            return
+        }
+        def needUser = userService.findById(userId)
+        if (!needUser) {
+            return
+        }
+        UserToken userToken = userTokenService.findLastToken(needUser)
+        if (!userToken) {
+            return
+        }
+        return userToken.token
     }
 }

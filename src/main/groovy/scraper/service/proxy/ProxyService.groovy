5b7dc7bb830164981ce145d898c0309d1bb85fdb
sergey.tkachenko <serg.tkachenko@hotmail.com>
2020-12-19 19:23:40 +0200

    switch from scapoxy to proxy

--- src/main/groovy/scraper/service/proxy/ProxyService.groovy
+++ src/main/groovy/scraper/service/proxy/ProxyService.groovy
@@ -1,6 +1,5 @@
 package scraper.service.proxy
 
-import groovy.json.JsonSlurper
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.springframework.beans.factory.annotation.Value
@@ -11,30 +10,8 @@ class ProxyService {
 
     private Logger logger = LogManager.getRootLogger()
 
-    @Value('${proxy.service.url}')
-    String proxyServiceUrl
-
     @Value('${proxy.static.url}')
     String proxyStaticUrl
-
-    int CONNECT_TIMEOUT = 3 * 1000
-    int READ_TIMEOUT = 3 * 1000
-
-    /**
-     * @deprecated
-     */
-    ProxyModel getFastProxy() {
-        def data = new URL(proxyServiceUrl + "/proxy/fast")
-                .getText(connectTimeout: CONNECT_TIMEOUT, readTimeout: READ_TIMEOUT)
-        if (!data) {
-            logger.error("proxy not found, proxyServiceUrl: ${proxyServiceUrl}")
-            return null
-        }
-        def jsonSlurper = new JsonSlurper()
-        def object = jsonSlurper.parseText(data)
-        logger.info("proxy found, data: ${data}")
-        return object as ProxyModel
-    }
 
     String getStaticHttpProxy() {
         return proxyStaticUrl
